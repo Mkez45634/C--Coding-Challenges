b0bd716f94252f74f76c4233111f2cbb9f9d3b26
Mkez45634 <18235891+Mkez45634@users.noreply.github.com>
2021-11-27 18:08:39 +0000

    Add files via upload

---
+++ ConditionalStatementExercises/ConditionalStatementExercises/Program.cs
@@ -0,0 +1,142 @@
+﻿class Program 
+{
+    static int AbsoluteValue(int x) 
+    {
+        return Math.Abs(x);
+    }
+
+    static int DivisibleBy2or3(int x, int y) 
+    {
+        // if both integers are divisble by 2 or 3 return the multiplication, else the sum.
+        if (((x % 2 == 0) || (x % 3 == 0)) && ((y % 2 == 0) || (y % 3 == 0)))
+        {
+            return x * y;
+        }
+        else
+        {
+            return x + y;
+        }
+    }
+    static bool IsUppercase(string s)
+    {
+        return (s == s.ToUpper()) && (s.All(Char.IsLetter));
+    }
+
+    static bool GreatherThanThird(int[] x) 
+    {
+        //check if the sum or multiplication of the first two in the array is greater than the third
+        if ((x[0] + x[1] > x[2]) || (x[0] * x[1] > x[2]))
+        {
+            return true;
+        }
+        else 
+        {
+            return false;
+        }
+    }
+
+    static bool IsEven(int x) 
+    {
+        if (x % 2 == 0)
+        {
+            return true;
+        }
+        else 
+        {
+            return false;
+        }
+    }
+
+    static bool IsAscending(int[] x)
+    {
+        if (x[0] <= x[1] && x[1]<= x[2])
+        {
+            return true;
+        }
+        else 
+        {
+            return false;
+        }
+    }
+
+    static bool HasNeighbour(string s) 
+    {
+        return s[0] == s[1] - 1 || s[0] == s[1] + 1 || s[2] == s[1] - 1 || s[2] == s[1] + 1;
+    }
+
+    static string PositiveNegativeOrZero(int x)
+    {
+        if (x > 0)
+        {
+            return "Postive";
+        }
+        else if( x == 0) 
+        {
+            return "Zero";
+        }
+        else
+        {
+            return "Negative";
+        }
+    }
+
+    static bool IsLeapYear(int x) 
+    {
+        /*  A Year that is divisible by 4 is a leap year
+         * except if it is divisible by 100
+         * except if it is divisible by 400         
+         */
+
+        if ((x % 4 == 0 && x % 100 != 0) || x % 400 == 0)
+        {
+            return true;
+        }
+        else 
+        {
+            return false;
+        }
+    }
+
+    static bool ContainsThree(int x) 
+    {
+        // See if the integer contains the digit 3 without using inbuilt methods or non int types.
+        while (x > 0) 
+        {
+            if (x % 10 == 3)  //check if the current last digit is three
+            {
+                return true;
+            }
+
+            x /= 10; // remove the lowest digit on the integer
+        }
+
+        return false;
+    }
+
+static void Main()
+    {
+        Console.WriteLine(AbsoluteValue(6832));
+        Console.WriteLine(AbsoluteValue(-392));
+        Console.WriteLine(DivisibleBy2or3(15, 30));
+        Console.WriteLine(DivisibleBy2or3(2, 90));
+        Console.WriteLine(DivisibleBy2or3(7, 12));
+        Console.WriteLine(IsUppercase("xyz"));
+        Console.WriteLine(IsUppercase("DOG"));
+        Console.WriteLine(IsUppercase("L9#"));
+        Console.WriteLine(GreatherThanThird(new int[] { 2, 7, 12 }));
+        Console.WriteLine(GreatherThanThird(new int[] { -5, -8, 50 }));
+        Console.WriteLine(IsEven(721));
+        Console.WriteLine(IsEven(1248));
+        Console.WriteLine(IsAscending(new int[] { 3, 7, 10 }));
+        Console.WriteLine(IsAscending(new int[] {74, 62, 99}));
+        Console.WriteLine(HasNeighbour("ABC"));
+        Console.WriteLine(HasNeighbour("GMV"));
+        Console.WriteLine(PositiveNegativeOrZero(12));
+        Console.WriteLine(PositiveNegativeOrZero(0));
+        Console.WriteLine(PositiveNegativeOrZero(-6));
+        Console.WriteLine(IsLeapYear(2016));
+        Console.WriteLine(IsLeapYear(2018));
+        Console.WriteLine(ContainsThree(1234567890));
+        Console.WriteLine(ContainsThree(87501));
+    }
+}
---
+++ LoopExercises/LoopExercises/Program.cs
@@ -0,0 +1,114 @@
+﻿class Program 
+{
+    static void TimesTable() 
+    {
+        //when called prints the 1 - 10 times table, for the first 10 muliplications
+        for (int i = 1; i <= 10; i++) 
+        {
+            for (int j = 1; j <= 10; j++) 
+            {
+                Console.Write($" {i * j}");
+            }
+            Console.Write("\n");
+        }
+    }
+
+    static double FractionsSum(int x) 
+    {
+        double result = 0;
+        for (int i = 1; i<=x; i++) 
+        {
+            result += (1 / (double)(i * i));
+        }
+        return result;
+    }
+
+    static int[] ArraySort(int[] x) 
+    {
+        int length = x.Length;
+        for (int i = 1; i < length; i++) 
+        {
+            int temp = x[i];
+            int j = i - 1;
+
+            while (j>=0 && x[j] > temp) 
+            {
+                x[j + 1] = x[j];
+                j = j - 1;
+            }
+
+            x[j + 1] = temp;
+        }
+
+        return x;  
+    }
+
+    static int BiggestNum(int[] x) 
+    {
+        x = ArraySort(x);
+        return x[x.Length - 1];
+    }
+
+    static int Sevens(int[] x)
+    {
+        int SevensCount = 0;
+
+        for (int i = 0; i < x.Length - 1; i++) 
+        {
+            if ( x[i] == 7  && x[i+1] == 7) 
+            {
+                SevensCount++;
+            }
+        }
+
+        return SevensCount;
+    }
+
+    static bool ThreeAdjacent(int[] x) 
+    {
+        bool Flag = false;
+
+        for (int i = 1; i < x.Length - 2; i++) 
+        {
+            if (x[i-1] + 1 == x[i] && x[i] == x[i+1] -1) 
+            {
+                Flag = true;
+            }
+        }
+
+        return Flag;
+    }
+
+    static string Evens() 
+    {
+        string s = "";
+
+        for (int i = 1; i < 100; i++) 
+        {
+            if ( i % 2 == 0)  //it is even
+            {
+                s += i + " ";
+            }
+        }
+
+        return s;
+    }
+
+    static void Main() 
+    {
+        TimesTable();
+        Console.WriteLine(FractionsSum(3));
+        Console.WriteLine(FractionsSum(5));
+        Array.ForEach(ArraySort(new int[] { 9, 5, 7, 2, 1, 8 }), Console.Write);
+        Console.WriteLine();
+        Console.WriteLine(BiggestNum(new int[] { 9, 5, 7, 2, 1, 8 }));
+        Console.WriteLine(BiggestNum(new int[] { -34, -54, -7, -40, -123, -99 }));
+        Console.WriteLine(Sevens(new int[] { 7, 7, 8, 4, 3, 7, 2, 1, 0, 7 }));
+        Console.WriteLine(Sevens(new int[] { 4, 7, 8, 2, 0, 5, 2, 7, 5, 8 }));
+        Console.WriteLine(Sevens(new int[] { 7, 7, 7, 0, 2, 6, 4, 8, 6, 5, 2, 7, 7 }));
+        Console.WriteLine(ThreeAdjacent(new int[] { 7, 8, 9, 2, 4, 5, 0 }));
+        Console.WriteLine(ThreeAdjacent(new int[] { -9, 0, -1, -6, -5, -4, -8, 0 }));
+        Console.WriteLine(ThreeAdjacent(new int[] { 15, 17, 14, 11, 18, 19, 16, 16 }));
+        Console.WriteLine(Evens());
+    }
+}
